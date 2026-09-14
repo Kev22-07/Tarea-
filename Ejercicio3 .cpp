bc0114e6284d72da88cad479f7de946594b96bd5
Kev22-07 <erik.vinq@gmail.com>
2026-09-14 09:39:05 -0500

    Create Ejercicio3 .cpp
    Ejercicio 3

---
+++ Ejercicio3 .cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+unsing namespace std;
+int main() {
+  int a,b;
+  cout<<"Ingrese Dos Numeros";
+  cin>>a>>b;
+  int mayor=(a>b)*a+(b>=a)*b;
+  int menor=(a<b)*a+(b<=a)*b;
+  cout <<"mayor";
+  cout <<"menor";
+  return 0
+    }
