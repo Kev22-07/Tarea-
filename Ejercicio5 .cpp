379ff226dc40e84acef1394cac5878a2179829b5
Kev22-07 <erik.vinq@gmail.com>
2026-09-14 09:54:53 -0500

    Create Ejercicio5 .cpp
    Ejercicio 5

---
+++ Ejercicio5 .cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+unsing namespace std;
+int main() {
+  int n;
+cin>>n;
+IF(N>0);
+{
+cout<<"positivo";
+}
+else
+{
+cout<<"negativo";
+}
+IF(N==o);
+{
+cout <<"nulo";
+}
+return 0
+  }
